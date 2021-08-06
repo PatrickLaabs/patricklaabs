c500f5ec6003db646fd319c45e0afc445c267afc
PatrickLaabs <patrick.laabs@me.com>
2021-08-06 09:09:12 +0200

    More context to the readme

--- update/main.go
+++ update/main.go
@@ -11,8 +11,8 @@ func makeReadme(filename string) error {
 	date := time.Now().Format("2 Jan 2006")
 
 	// Whisk together static and dynamic content until stiff peaks form
-	hello := "### Hello! I'm Patrick.\n\nGo enthusiast and just getting started on my journey to DevOps :) \n\n Will this be automated? :P"
-	updated := "<sub>Last updated by magic on " + date + ".</sub>"
+	hello := "### Hello! I'm Patrick.\n\nGo enthusiast and just getting started on my journey to DevOps :) \n\nCurrently I am building my Jenkins pipeline with some GitHub Actions & Workflows to have a full ci/cd pipeline up and running for my golang application.\nThe Golang Application is not ready yet and currently I am only getting the tests running.\nNext thing that I want to have running is Goreleaser... but this may take some time :D"
+	updated := "<sub>Lastest update on " + date + ".</sub>"
 	data := fmt.Sprintf("%s\n\n\n%s\n", hello, updated)
 
 	// Prepare file with a light coating of os
