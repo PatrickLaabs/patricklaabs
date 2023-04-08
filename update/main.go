1cffb07d90392e71ac7c4877de797978a324d9b2
Patrick Laabs <patrick.laabs@me.com>
2023-04-08 15:14:59 +0200

    Update main.go

--- update/main.go
+++ update/main.go
@@ -12,13 +12,8 @@ func makeReadme(filename string) {
 	date := time.Now().Format("2 Jan 2006")
 
 	// Whisk together static and dynamic content until stiff peaks form
-	hello := "### Hello! I'm Patrick.\n\n" +
-		":v: Go enthusiast\n\n:muscle: DevOps lover\n\n" +
+	hello := ":v: Go enthusiast\n\n:muscle: DevOps lover\n\n" +
 		"---\n\n" +
-		"I build a pipeline for/in GitHub Actions for golang projects.\n The Goal is to have a clean and easy to set up pipeline" +
-		"for almost every golang project\nso no more rebuilding the pipe for every project.\n\n" +
-		"Well, lets see if this works out as intended. At least its empowered by goreleaser :heart:\n" +
-		"[https://github.com/PatrickLaabs/golang-pipeline](https://github.com/PatrickLaabs/golang-pipeline)\n\n" +
 		"![Image alt text](/images/gopher_with_coffee.gif)"
 	updated := "<sub>Lastest update on " + date + ".</sub>"
 	data := fmt.Sprintf("%s\n\n\n%s\n", hello, updated)
